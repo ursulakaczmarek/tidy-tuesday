80b6de44f3e3c0a66c930ca50b23df714119020a
ursulakaczmarek <ursula.kaczmarek@gmail.com>
2024-01-08 21:54:16 -0500

    updating week 1 code

--- 2024/tidy_tuesday_1.R
+++ 2024/tidy_tuesday_1.R
@@ -1,32 +1,49 @@
 library(xml2)
-library(methods)
 library(tidyverse)
 library(lubridate)
 library(ggplot2)
 
+# export instructions here: https://support.apple.com/guide/iphone/share-your-health-data-iph5ede58c3d/ios
 xml <- read_xml("export.xml")
 
 # filter to distance and date data
 workouts <- xml_find_all(xml, ".//WorkoutStatistics[@type = 'HKQuantityTypeIdentifierDistanceWalkingRunning']")
 
-# filter to resting heart rate (@value = '1')
-bpms <- xml_find_all(xml, ".//Record[MetadataEntry/@key = 'HKMetadataKeyHeartRateMotionContext' and MetadataEntry/@value = '1']")
+# filtering to sedentary heart rate (@value = '1' or '0', not '2') is not as accurate as the resting heart rate sampling
+# see https://developer.apple.com/documentation/healthkit/hkquantitytypeidentifier/2867756-restingheartrate
+# bpms <- xml_find_all(xml, ".//Record[MetadataEntry/@key = 'HKMetadataKeyHeartRateMotionContext' and MetadataEntry/@value != '2']")
+bpms <- xml_find_all(xml, ".//Record[@type = 'HKQuantityTypeIdentifierRestingHeartRate']")
 
-bpm_df <- tibble(bpm_date = xml_attr(bpms, "endDate") |> as.POSIXct(format = "%Y-%m-%d"),  
-       year_week = paste(year(bpm_date), week(bpm_date), sep = "-"),
-       bpm = xml_attr(bpms, "value")) |> 
-  group_by(bpm_date) |> 
-  slice_head(n = 2) |> # apple watch logs lots of readings, so limit to first two of the day
+# convert to tibbles
+bpm_df <- tibble(reading_date = xml_attr(bpms, "endDate") |> as.Date(format = "%Y-%m-%d"),  
+       bpm = as.integer(xml_attr(bpms, "value")))
+
+combined <- tibble(reading_date = xml_attr(workouts, "endDate") |> as.Date(format = "%Y-%m-%d"),  
+  distance = as.numeric(xml_attr(workouts, "sum"))) |> 
+  full_join(bpm_df, by = "reading_date") |> 
+  replace_na(list(distance = 0)) |> 
+  group_by(paste0(week(reading_date), year(reading_date))) |> 
+    mutate(weekly_distance = sum(distance)) |> 
   ungroup()
 
-combined <- tibble(end_date = xml_attr(workouts, "endDate") |> as.POSIXct(format = "%Y-%m-%d"),  
-  year_week = paste(year(end_date), week(end_date), sep = "-"),
-  distance = xml_attr(workouts, "sum") |> as.numeric()) |> 
-  group_by(year_week) |> 
-  transmute(weekly_distance = sum(distance)) |> 
-  ungroup() |> 
-  full_join(bpm_df, by = "year_week")
-  
+# correlation coefficient
+coeff <- cor(combined$bpm, combined$weekly_distance, use = "complete.obs")
+
+# for plotting, secondary axis is estimated relationship between bpm+mileage
+plot <- ggplot(data = combined, aes(reading_date, bpm, color = bpm)) + 
+  geom_point(size = 1) +
+  geom_smooth(method = "lm", se = FALSE, color = "#102349", linewidth = 0.5) +
+  scale_color_gradient(name = "resting heart rate (bpm)", low = "#046b99", high = "#d00060") +
+  scale_x_date() +
+  scale_y_continuous(breaks = scales::pretty_breaks(n = 10),
+                     sec.axis = sec_axis(~./2, name = "rolling mean weekly mileage")) +
+  theme_minimal(base_family = "mono") +
+  labs(title = "resting heart rate plotted against average run miles/week", 
+       subtitle = sprintf("correlation coefficient: %f", coeff),
+       x = "reading date")
+
+plot +
+  geom_line(aes(y = zoo::rollmean(weekly_distance*2, k = 3, fill = mean(distance), align = "right")), color = "#d00060")
 
 
 
